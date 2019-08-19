b9460870a01bcefd33dc3b3655c407a8cc21f548
Aaron Lanier <alanier.william@gmail.com>
2019-08-19 13:24:52 -0400

    created database and table

---
+++ bamazonDB.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR (45) NULL,
+  department_name VARCHAR (45) NULL,
+  price INT (9) NOT NULL,
+  stock_quantity INT (9) NOT NULL,
+  PRIMARY KEY (item_id)
+);
