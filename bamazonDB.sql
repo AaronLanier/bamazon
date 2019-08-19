05749c4cf3993317b9f317a3d3a01d5633cef9af
Aaron Lanier <alanier.william@gmail.com>
2019-08-19 13:44:13 -0400

    populated database with 10 products usint INSERT INTO

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -8,7 +8,37 @@ CREATE TABLE products (
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR (45) NULL,
   department_name VARCHAR (45) NULL,
-  price INT (9) NOT NULL,
+  price DECIMAL(10,2) NULL,
   stock_quantity INT (9) NOT NULL,
   PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("airpods", "tech", 159.99, 467);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("nerf gun", "toys", 19.99, 635);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("PS4", "video games", 299.99, 143);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("coffee maker", "appliances", 49.99, 215);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("laptop", "tech", 499.99, 376);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("fishing pole", "hunting & fishing", 69.99, 189);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("shoes", "apparel", 199.99, 376);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iphone", "tech", 999.99, 805);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("hat", "apparel", 15.00, 923);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("gun", "hunting & fishing", 1999.99, 756);
