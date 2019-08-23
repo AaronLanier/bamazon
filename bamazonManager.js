22f1eb2afa1de33244fdb67c1700c81225fd0222
Aaron Lanier <alanier.william@gmail.com>
2019-08-22 23:23:47 -0400

    psudocoded manager challenge

---
+++ bamazonManager.js
@@ -0,0 +1,33 @@
+//require mysql and inquirer
+
+//connect to mysql with .connect and set up database
+
+// function that gets product from DB. 
+    // loads manager options using inquirer prompt
+
+
+// function that loads manager options
+     // type name choises message
+    //switch case
+
+// function that selects all low inventory products
+    //connect to database 
+    //select everything from products that have a quantity <= 5
+    //call our load manager function
+
+// function that adds to inventory 
+    // prompt with type, name, message, 
+    // if statement that says if...
+    // ...product with id is valid then ask how many items they want to buy
+    // else statement calling load manager function
+
+// function that acts as the manager about the new product
+    // prompt with an array of objects
+    // each object has type, name, product, and quantity
+    // call add product function
+
+// function that adds product
+    // connects to database
+    // inserts new product
+    // call load manager function
+
